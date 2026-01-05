97d954bfaa18ca76af9274e8f96d594e36bc2d12
virtualformacion <virtualformacion47@gmail.com>
2026-01-05 19:04:25 +0000

    Actualizar usuarios (delete) desde admin

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // ========== USUARIOS AUTORIZADOS ==========
 const USERS = [
-    { username: "nyx112", password: "2212", expiresAt: new Date("2025-12-26"), emails: [] },
     { username: "cliea1", password: "2212", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "andres", password: "1234", expiresAt: new Date("2026-01-14"), emails: ["codecastle006@gmail.com","andres@yopmail.com"] }
 ];
